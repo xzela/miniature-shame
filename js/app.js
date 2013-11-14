8838216f43299a909c96c8ddb88e895ff60d604f
Zeph Lafassett <zeph@lafassett.com>
2013-11-14 09:49:46 -0800

    got it mostly working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 // Define the Ember application
-App = Ember.Application.create();
+App = Ember.Application.create({
+	version: '0.091.91'
+});
 
 App.Router.map(function() {
 	this.resource('about');
 	this.resource('posts');
 });
+
+App.List = [{
+	name: "my awesome name"
+},
+{
+	name: "some other name"
+}];
