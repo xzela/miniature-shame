832c431759433c541ac31ee61c557191ea1e5e11
Zeph Lafassett <zeph@lafassett.com>
2013-11-13 20:52:15 -0800

    adding bootstap

--- js/app.js
+++ js/app.js
@@ -1,21 +1,7 @@
 // Define the Ember application
-Todos = Ember.Application.create();
+App = Ember.Application.create();
 
-// Ember Object
-Todos.todo = Ember.Object.extend({
-	title: null,
-	isDone: false
-});
-
-Todos.Controller = Ember.Object.create({
-	// Ember Array A()?
-	todos: Ember.A(),
-
-	// init property
-	init: function() {
-		var items = this.get('todos');
-		// add some items
-		items.addObject(Todos.todo.create({title: 'Title 1'}));
-		items.addObject(Todos.todo.create({title: 'Title 2'}));
-	}
+App.Router.map(function() {
+	this.resource('about');
+	this.resource('posts');
 });
