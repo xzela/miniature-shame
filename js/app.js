9199107128673a60f882bd716357fa72289f88f9
Zeph Lafassett <zeph@lafassett.com>
2013-11-27 12:57:07 -0800

    redoing stuff

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
 // Define the Ember application
-App = Ember.Application.create({
-	version: '0.091.91'
+var Roller = Ember.Application.create({
+    LOG_TRANSITIONS: true,
+    LOG_BINDINGS: true,
+    LOG_VIEW_LOOKUPS: true,
+    LOG_STACKTRACE_ON_DEPRECATION: true,
+    LOG_VERSION: true,
+    debugMode: true
 });
 
-App.Router.map(function() {
-	this.resource('about');
-	this.resource('posts');
+// override index route to roll
+Roller.IndexRoute = Ember.Route.extend({
+    redirect: function () {
+        this.transitionTo("roll");
+    }
 });
 
-App.List = [{
-	name: "my awesome name"
-},
-{
-	name: "some other name"
-}];
+//
+Roller.RollRoute = Ember.Route.extend({
+    model: function() {
+        return [];
+    },
+
+    setupController: function(controller, model) {
+        controller.set("content", model);
+    }
+});
+
+Roller.Router.map(function () {
+    this.resource("roll");
+});
+
+Roller.Roll = Ember.Object.extend({
+    diceNumber: 0,
+    totalRolls: 0,
+    numberOfRolls: 0,
+
+    proportion: function() {
+        var width = 50 + parseInt(400 * this.get("numberOfRolls") / this.get("totalRolls"));
+        return "width: " + width + "px;";
+    }.property("totalRolls", "numberOfRolls")
+});
